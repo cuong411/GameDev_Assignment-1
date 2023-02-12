e7cd17a0d7d9b4fdc34f7a9099af0178b5d61946
cuong411 <cuongtruong29122002@gmail.com>
2023-02-12 19:10:53 +0700

    visual added

--- mygame.py
+++ mygame.py
@@ -1,5 +1,6 @@
 import pygame
 import os
+import random
 
 from os import path
 from pygame.locals import *
@@ -12,28 +13,30 @@ whack2 = pygame.transform.scale(pygame.image.load(os.path.join('img', 'whack2.pn
 zb = pygame.transform.scale(pygame.image.load(os.path.join('img', 'std_zombie.png')), (100, 100))
 bg = pygame.transform.scale(pygame.image.load(os.path.join('img', 'lawn.png')), (800, 600))
 
+# variables
 WIDTH = 800
 HEIGHT = 600
 FPS = 60
 BG = (17, 166, 41)
 COL = 3
 ROW = 3
+
 mouse_pos = (0, 0)
 cursor = whack1
 
-def zombie_generator():
-    
-
+# function
 def draw_dirt():
     x,y = 0,0
     for row in range(ROW):
         x = 0
         for cal in range(COL):
-            screen.blit(zb, (x * 200 + 150, y * 200 + 35))
+            #screen.blit(zb, (x * 200 + 150, y * 200 + 35))
             screen.blit(dirt, (x * 200 + 115, y * 200 + 120))
             x += 1
         y += 1
 
+def zombie_generator():
+    
 
 pygame.init()
 
@@ -43,6 +46,7 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 pygame.mouse.set_visible(False)
 
+# gameloop
 run = True
 while run:
     for event in pygame.event.get():
@@ -58,5 +62,7 @@ while run:
     screen.blit(bg, (0, 0))
     draw_dirt()
     screen.blit(cursor, (mouse_pos[0] - 25, mouse_pos[1] - 25))
+
+    clock.tick(FPS)
 
 pygame.quit()
